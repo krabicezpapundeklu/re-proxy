bd6e064798279c517a604292d50e17a043cd5846
JF <jaroslav.flieger@monster.com>
2021-09-22 04:04:19 +0200

    better map handling + request handlers

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
 const SOURCE = 'https://XXX-XXX/XXX';
 const TARGET = 'c:/XXX/webapp';
 
+const READ_FROM_TARGET_IF_URL_MATCHES = /\.js($|\?)/;
+
+const REQUEST_HANDLERS = [
+    {
+        method: 'get', pathPattern: /\/XXX$/, handler: (req, res) => {
+            res.json({fakeData: []});
+        }
+    },
+    {
+        method: 'post', pathPattern: /\/XXX$/, handler: (req, res) => {
+            res.json({fakeData: []});
+        }
+    }
+];
+
 const TARGET_PORT = 3000;
 const TARGET_HOST = `http://localhost:${TARGET_PORT}`;
 
 if (typeof chrome !== 'undefined') {
     chrome.webRequest.onBeforeRequest.addListener(details => {
-        let url = details.url;
-        let targetUrl;
+        let path = new URL(details.url).pathname;
+        let redirect = false;
 
-        if (url.match(/\.js($|\?)/)) {
-            targetUrl = TARGET_HOST + url.substr(SOURCE.length);
-        } else if (url.indexOf('/XXX') !== -1) {
-            targetUrl = TARGET_HOST + '/fakeXXX';
+        for (const handler of REQUEST_HANDLERS) {
+            if (path.match(handler.pathPattern)) {
+                redirect = true;
+                break;
+            }
         }
 
-        if (targetUrl) {
-            return {redirectUrl: targetUrl};
+        if (path.match(READ_FROM_TARGET_IF_URL_MATCHES)) {
+            redirect = true;
         }
-    }, {urls: [SOURCE + '/*']}, ['blocking']);
+
+        if (redirect) {
+            return {redirectUrl: TARGET_HOST + details.url.substr(SOURCE.length)};
+        }
+    }, {urls: [SOURCE + '*']}, ['blocking']);
 
     chrome.webRequest.onHeadersReceived.addListener(details => {
         let headers = details.responseHeaders;
@@ -31,34 +51,49 @@ if (typeof chrome !== 'undefined') {
         }
 
         return {responseHeaders: headers};
-    }, {urls: [SOURCE + '/*']}, ['blocking', 'responseHeaders']);
+    }, {urls: [SOURCE + '*']}, ['blocking', 'responseHeaders']);
 } else {
     const express = require('express');
     const cors = require('cors');
     const fs = require('fs');
+    const path = require('path');
 
     const app = express();
 
     app.use(cors());
 
+    for (const handler of REQUEST_HANDLERS) {
+        app[handler.method](handler.pathPattern, handler.handler);
+    }
+
     app.get(/.*/, (req, res) => {
-        fs.readFile(`${TARGET}/${req.path}`, 'utf-8', (err, data) => {
-            if (err) {
-                res.sendStatus(404);
+        const targetPath = path.join(TARGET, req.path);
+
+        if (req.path.endsWith('.css') || req.path.endsWith('.js')) {
+            const mapPath = targetPath + '.map';
+
+            if (fs.existsSync(mapPath)) {
+                fs.readFile(targetPath, 'utf-8', (err, data) => {
+                    if (err) {
+                        res.sendStatus(404);
+                        return;
+                    }
+
+                    const mapName = path.basename(mapPath);
+                    const hostMapPath = `${TARGET_HOST}${req.path}.map`;
+
+                    data = data.replace(`sourceMappingURL=${mapName}`, `sourceMappingURL=${hostMapPath}`);
+
+                    res.type(path.extname(req.path));
+                    res.send(data);
+                });
+
                 return;
             }
+        }
 
-            let file = req.path.substring(req.path.lastIndexOf('/') + 1);
-
-            data = data.replace(`sourceMappingURL=${file}.map`, `sourceMappingURL=${TARGET_HOST}${req.path}.map`);
-
-            res.send(data);
-        });
+        res.sendFile(targetPath);
     })
-
-    app.post('/fakeXXX', (req, res) => {
-        res.json({fakeData: []});
-    });
 
     app.listen(TARGET_PORT);
 }
